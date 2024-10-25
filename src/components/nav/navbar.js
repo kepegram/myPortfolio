2ce613b23e3ec75fd2af6895762f7c65ee2590f1
Kadin Pegram <kpegra1@students.towson.edu>
2024-10-25 15:21:16 -0500

    Updating headshot/resume

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-scroll";
 import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../../assets/kp-logo.png";
-import resume from "../../assets/pegram-resume-new.pdf";
+import resume from "../../assets/pegram-2024-resume.pdf";
 import "./navbar.css";
 
 const Navbar = () => {
